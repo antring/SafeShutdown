72c5dea00fb4160cb019353dfc47849b01bd035c
antring <emauring@hotmail.com>
2016-12-26 02:30:42 +0100

    console logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from subprocess import check_output
 from os import system
 
 import time
+
 import datetime
 
 
@@ -24,12 +25,16 @@ def main():
         if has_ping("8.8.8.8") or has_ping("www.vg.no"):
             missing_link = False
             times_missed = 0
+            print("(*) - Link active - " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             logfile.write("(*) - Link active - " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
             logfile.close()
 
         else:
             missing_link = True
             times_missed += 1
+            print(
+                "(*) - Link missing! - Missed contact: " + str(times_missed) + " - " + datetime.datetime.now().strftime(
+                    '%Y-%m-%d %H:%M:%S'))
             logfile.write(
                 "(*) - Link missing! - Missed contact: " + str(times_missed) + " - " + datetime.datetime.now().strftime(
                     '%Y-%m-%d %H:%M:%S') + "\n")
@@ -38,6 +43,7 @@ def main():
         if missing_link and times_missed == 3:
             logfile.write("(!) Shutdown initiated - " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
             logfile.close()
+            print(("(!) Shutdown initiated - " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
             system("sudo shutdown")
 
         time.sleep(300)
